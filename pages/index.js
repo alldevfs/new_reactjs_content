352f3528a8d927e76a9e1737051f779932f88e0e
Allan Victor <alldevfs@outlook.com>
2020-11-09 23:22:28 -0300

    first page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+	return (
+    <div>
+		<h1>React rapidinho.</h1>
+		<h3>Utilizando o Water.css</h3>
+	<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed.</h2>
+	</div>
+	)
+}
+
+export default Home
