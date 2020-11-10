7690b12cfa3a73e151c14553df3685aef025447e
Allan Victor <alldevfs@outlook.com>
2020-11-09 23:31:45 -0300

    update page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ function Home() {
     <div>
 		<h1>React rapidinho.</h1>
 		<h3>Utilizando o Water.css</h3>
-	<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed.</h2>
+	<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. SedLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed.</h2>
 	</div>
 	)
 }
